087ea323bf2573e88ee179b8ab7f2bfb311ee0e6
calvin cushman and Jayeson Kiyabu <calvin@>
2017-01-10 16:15:15 -0800

    add toggle function to all buttons

---
+++ js/scripts.js
@@ -0,0 +1,77 @@
+$(function(){
+  $("button#0").click(function() {
+    $(".def0-visible").toggle();
+  });
+
+  $("button#1").click(function() {
+    $(".def1-visible").toggle();
+  });
+
+  $("button#2").click(function() {
+    $(".def2-visible").toggle();
+  });
+
+  $("button#3").click(function() {
+    $(".def3-visible").toggle();
+  });
+
+  $("button#4").click(function() {
+    $(".def4-visible").toggle();
+  });
+
+  $("button#5").click(function() {
+    $(".def5-visible").toggle();
+  });
+
+  $("button#6").click(function() {
+    $(".def6-visible").toggle();
+  });
+
+  $("button#7").click(function() {
+    $(".def7-visible").toggle();
+  });
+
+  $("button#8").click(function() {
+    $(".def8-visible").toggle();
+  });
+
+  $("button#9").click(function() {
+    $(".def9-visible").toggle();
+  });
+
+  $("button#10").click(function() {
+    $(".def10-visible").toggle();
+  });
+
+  $("button#11").click(function() {
+    $(".def11-visible").toggle();
+  });
+
+  $("button#12").click(function() {
+    $(".def12-visible").toggle();
+  });
+
+  $("button#13").click(function() {
+    $(".def13-visible").toggle();
+  });
+
+  $("button#14").click(function() {
+    $(".def14-visible").toggle();
+  });
+
+  $("button#15").click(function() {
+    $(".def15-visible").toggle();
+  });
+
+  $("button#16").click(function() {
+    $(".def16-visible").toggle();
+  });
+
+  $("button#17").click(function() {
+    $(".def17-visible").toggle();
+  });
+
+  $("button#18").click(function() {
+    $(".def18-visible").toggle();
+  });
+});
