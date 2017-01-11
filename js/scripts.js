869c432e028d3d05c9893574e4d53712120d8a8b
calvin cushman and Jayeson Kiyabu <jayeson@>
2017-01-10 16:50:25 -0800

    add fadeOut to inactive text

--- js/scripts.js
+++ js/scripts.js
@@ -1,77 +1,102 @@
+var fadeSpeed = 400;
+
 $(function(){
   $("button#0").click(function() {
+    $(".col-md-7 div").fadeOut();
     $(".def0-visible").toggle();
+
   });
 
   $("button#1").click(function() {
+    $(".col-md-7 div").fadeOut();
     $(".def1-visible").toggle();
+
   });
 
   $("button#2").click(function() {
+    $(".col-md-7 div").fadeOut();
     $(".def2-visible").toggle();
   });
 
   $("button#3").click(function() {
+    // $(".col-md-7>ul>li>div").fadeOut(fadeSpeed);
+    $(".col-md-7 div").fadeOut();
     $(".def3-visible").toggle();
   });
 
   $("button#4").click(function() {
+    $(".col-md-7 div").fadeOut();
+    // $(".col-md-7 div").show();
     $(".def4-visible").toggle();
   });
 
   $("button#5").click(function() {
+    $(".col-md-7 div").fadeOut();
     $(".def5-visible").toggle();
   });
 
   $("button#6").click(function() {
+    $(".col-md-7 div").fadeOut();
     $(".def6-visible").toggle();
   });
 
   $("button#7").click(function() {
+    $(".col-md-7 div").fadeOut();
     $(".def7-visible").toggle();
   });
 
   $("button#8").click(function() {
+    $(".col-md-7 div").fadeOut();
     $(".def8-visible").toggle();
   });
 
   $("button#9").click(function() {
+    $(".col-md-7 div").fadeOut();
     $(".def9-visible").toggle();
   });
 
   $("button#10").click(function() {
+    $(".col-md-7 div").fadeOut();
     $(".def10-visible").toggle();
   });
 
   $("button#11").click(function() {
+    $(".col-md-7 div").fadeOut();
     $(".def11-visible").toggle();
   });
 
   $("button#12").click(function() {
+    $(".col-md-7 div").fadeOut();
     $(".def12-visible").toggle();
   });
 
   $("button#13").click(function() {
+    $(".col-md-7 div").fadeOut();
     $(".def13-visible").toggle();
   });
 
   $("button#14").click(function() {
+    $(".col-md-7 div").fadeOut();
     $(".def14-visible").toggle();
   });
 
   $("button#15").click(function() {
+    $(".col-md-7 div").fadeOut();
     $(".def15-visible").toggle();
   });
 
   $("button#16").click(function() {
+    $(".col-md-7 div").fadeOut();
     $(".def16-visible").toggle();
   });
 
   $("button#17").click(function() {
+    $(".col-md-7 div").fadeOut();
     $(".def17-visible").toggle();
   });
 
   $("button#18").click(function() {
+    $(".col-md-7 div").fadeOut();
     $(".def18-visible").toggle();
   });
 });
